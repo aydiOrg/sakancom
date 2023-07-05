db71e0278746235647758738dd1f913ba8a3329b
bara <s12010060@stu.najeh.edu>
2023-07-05 15:58:45 +0300

    5/7 3:58PM

--- src/main/java/com/example/sakankom/ResidenceHandler.java
+++ src/main/java/com/example/sakankom/ResidenceHandler.java
@@ -2,15 +2,25 @@ package com.example.sakankom;
 
 import com.example.sakankom.OwnerFiles.House;
 import com.example.sakankom.OwnerFiles.Residence;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class ResidenceHandler implements Initializable {
+    @FXML
+    private ResidencesHandler residencesHandler;
 
     @FXML
     private Label locationField;
@@ -20,7 +30,8 @@ public class ResidenceHandler implements Initializable {
 
     @FXML
     private Label residenceName;
-
+    @FXML
+    private VBox residenceCard;
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
@@ -31,4 +42,5 @@ public class ResidenceHandler implements Initializable {
         ownerName.setText(residence.getOwnerName());
         residenceName.setText(residence.getResidenceName());
     }
+
 }
--- src/main/java/com/example/sakankom/ResidencesHandler.java
+++ src/main/java/com/example/sakankom/ResidencesHandler.java
@@ -1,35 +1,33 @@
 package com.example.sakankom;
 
+import com.example.sakankom.OwnerFiles.House;
 import com.example.sakankom.OwnerFiles.Residence;
+import io.github.palexdev.materialfx.controls.MFXButton;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
+import oracle.jdbc.OracleConnectionWrapper;
 
+import java.io.IOException;
 import java.net.URL;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.ResourceBundle;
+import java.sql.*;
+import java.util.*;
 
 public class ResidencesHandler implements Initializable {
     @FXML
     private GridPane residenceContainer;
-    @FXML
-    private VBox residenceCard;
-    @FXML
-    private HBox residenceLayout;
-
     private List<Residence> residences;
+    private List<House> houses;
+    @FXML
+    private Label mainLabel;
 
+    private int totalTenants;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
@@ -43,8 +41,6 @@ public class ResidencesHandler implements Initializable {
             Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xepdb1", "sakankom", "12345678");
             Statement st = con.createStatement();
             Statement st2 = con.createStatement();
-            Statement st3 = con.createStatement();
-            Statement st4 = con.createStatement();
 
             rst = st.executeQuery("select owner_id, residence_id, residence_name, location from residence where isValid='1'");
 
@@ -68,6 +64,18 @@ public class ResidencesHandler implements Initializable {
                 ResidenceHandler residenceHandler = fxmlLoader.getController();
                 residenceHandler.setDate(residence);
 
+                for (Node node : residenceBox.getChildren()) {
+                    if (node instanceof HBox) {
+                        for (Node node2 : ((HBox) node).getChildren()) {
+                            if(node2 instanceof MFXButton button) {
+                                button.setOnAction(event -> {
+                                    showHouses(residence.getResidenceName());
+                                });
+                            }
+                        }
+                    }
+                }
+
                 if (column == 6) {
                     column = 0;
                     ++row;
@@ -81,6 +89,86 @@ public class ResidencesHandler implements Initializable {
             System.out.println(e);
         }
     }
+    public void showHouses(String residenceName){
+        residenceContainer.getChildren().clear();
+        houses = new ArrayList<>();
+        totalTenants = 0;
+        ResultSet rst, rst2;
 
+        try{
+            DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+            Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xepdb1", "sakankom", "12345678");
+            Statement st = con.createStatement();
+            Statement st2 = con.createStatement();
+
+            rst = st.executeQuery("SELECT residence_id FROM residence WHERE residence_name='" + residenceName + "' and isvalid='1'");
+            rst.next();
+            rst2 = st2.executeQuery("SELECT * FROM house WHERE residence_id='" + rst.getString("residence_id") + "' and isvalid='1'");
+
+            while (rst2.next()){
+                houses.add(new House(
+                        "House " + rst2.getString("house_id"),
+                        "/photos/" + rst2.getString("image"),
+                        rst2.getInt("price"),
+                        residenceName,
+                        rst2.getInt("floor_number")
+                ));
+
+                 totalTenants += rst2.getInt("reserved_capacity");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        mainLabel.setText("All houses of " + residenceName + " . " + houses.size() + " Houses with " + totalTenants + " person reserved.");
+
+        try{
+            ResultSet rst3, rst4;
+            Set<Integer> uniqueFloors = new HashSet<>();
+            int column = 1;
+            int row = 1;
+            try{
+                DriverManager.registerDriver(new oracle.jdbc.driver.OracleDriver());
+                Connection con = DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xepdb1", "sakankom", "12345678");
+                Statement st = con.createStatement();
+                Statement st2 = con.createStatement();
+
+                rst3 = st2.executeQuery("SELECT residence_id FROM residence WHERE isvalid='1' and residence_name='" + residenceName + "'");
+                rst3.next();
+                rst4 = st.executeQuery("SELECT floor_number FROM house WHERE isvalid='1' and residence_id='" + rst3.getString("residence_id") + "'");
+
+                while (rst4.next()) {
+                    uniqueFloors.add(rst4.getInt("floor_number"));
+                }
+                Integer[] FloorsArray = uniqueFloors.toArray(new Integer[0]);
+
+                Arrays.sort(FloorsArray);
+
+                for(int i = 0 ; i < FloorsArray.length ; i++){
+                    List<House> floorHouses = new ArrayList<>();
+                    for(House housee : houses){
+                        if(housee.getFloor() == FloorsArray[i]){
+                            floorHouses.add(housee);
+                        }
+                    }
+
+                    FXMLLoader fxmlLoader1 = new FXMLLoader();
+                    fxmlLoader1.setLocation(getClass().getResource("showHouses.fxml"));
+                    VBox floor = fxmlLoader1.load();
+                    ShowHousesHandler Handler2 = fxmlLoader1.getController();
+                    Handler2.setDate(floorHouses, residenceName);
+
+                    residenceContainer.add(floor, 0, row++);
+                    GridPane.setMargin(floor, new Insets(10));
+                    con.close();
+                }
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 }
